93527c429888936112a732feeb19a22993ecdc92
Isaac Kuehnle-Nelson <ikethepike@outlook.com>
2018-11-25 18:48:02 +0100

    Configured mix compilation correctly and added a landing page alongside introducing a scss framework

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,6 @@ const mix = require('laravel-mix')
 
 mix.browserSync('http://localhost:8080')
 
-mix
-  .js('resources/assets/js/app.js', 'public/js')
-  .extract(['vue', 'vue-router', 'axios', 'vuex'])
-  .sass('resources/assets/sass/app.scss', 'public/css')
+mix.js('resources/js/app.js', 'public/js')
+    .extract(['vue', 'vue-router', 'axios', 'vuex'])
+    .sass('resources/sass/app.scss', 'public/css')
